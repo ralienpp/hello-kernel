7226824e51c379d8e2ba68d05736e95ea9f991ca
U-SLIPPERY\alex <C:\Soft\TheBat\Mail>
2017-09-29 01:13:19 +0200

    simplified usermode tool CLI

--- testusermode.py
+++ testusermode.py
@@ -3,7 +3,14 @@ import sys
 
 # the module doesn't care what command you send it, but since an
 # integer is expected, let's give it one
-operation = int(sys.argv[-1])
+
+try:
+    operation = int(sys.argv[-1])
+except ValueError:
+    # if no command line arg was given, just use 3 as an ioctl
+    # operation; it doesn't really matter because the handler
+    # does not use this number at all
+    operation = 3
 
 
 oracle = open('/dev/oracle', 'rb')
